67debd77ffa5f7fb33e33c17887c94718bea4bfb
jackli0523 <j2379438361@gmail.com>
2024-04-23 08:05:03 -0700

    Revert "Initialized Repository"
    This reverts commit a9bc826466131a6914fe20e675817d7be5d60d72.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import Phaser from "phaser";
 
 // Load all of the scenes from ./scenes folder
 import { Start } from "./scenes/start";
-import { Title } from "./scenes/title";
 import { Options } from "./scenes/options";
 import { Credits } from "./scenes/credits";
 import { Combat } from "./scenes/combat";
-import { PostCombat } from "./scenes/post_combat";
 import { Inventory } from "./scenes/inventory";
 // Scene key name should be file name lower case and spaced
 
@@ -17,7 +15,7 @@ let config = {
   type: Phaser.CANVAS,
   width: 1280,
   height: 720,
-  scene: [Start, Title, Options, Credits, Combat, PostCombat, Inventory],
+  scene: [Start, Options, Credits, Combat, Inventory],
 };
 
 export const BSD = new Phaser.Game(config);
