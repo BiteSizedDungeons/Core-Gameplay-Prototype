b8418466518e9b1382743461bc7d7000b8daf494
Fallrain1763 <liushuchen842@icloud.com>
2024-04-22 20:23:42 -0700

    automode with 4 skills loop

---
+++ src/prefabs/characterElements.ts
@@ -0,0 +1,378 @@
+import { Action } from "./actions";
+
+const GAME_WIDTH = 1280;
+// const GAME_HEIGHT = 720;
+
+const BuffType = ["Attack", "Defense", "Speed"];
+
+export class Character {
+  healthbar: HealthBar;
+  actionbar: ActionBar;
+  curBuff: Map<string, boolean>;
+  curDebuff: Map<string, boolean>;
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    w: number,
+    h1: number,
+    v1: number,
+    h2: number,
+    v2: number
+  ) {
+    this.healthbar = new HealthBar(scene, x, y - h1 / 2, w, h1, v1);
+    this.actionbar = new ActionBar(scene, x, y + h2 / 2, w, h2, v2);
+    this.curBuff = new Map();
+    this.curDebuff = new Map();
+
+    for (const type of BuffType) {
+      this.curBuff.set(type, false);
+      this.curDebuff.set(type, false);
+    }
+  }
+
+  health(): number {
+    return this.healthbar.getValue();
+  }
+
+  damage(amount: number) {
+    let dmg = amount;
+    if (this.hasDebuff("Defense")) {
+      dmg = dmg * 1.5;
+    }
+    this.healthbar.decreaseBar(dmg);
+  }
+
+  heal(amount: number) {
+    this.healthbar.increaseBar(amount);
+  }
+
+  setPosition(xPos: number, yPos: number) {
+    const h1 = this.healthbar.background.height;
+    const h2 = this.actionbar.background.height;
+
+    this.healthbar.background.setPosition(xPos, yPos - h1 / 2);
+    this.healthbar.valueBar.setPosition(xPos, yPos - h1 / 2);
+    this.actionbar.background.setPosition(xPos, yPos + h2 / 2);
+    this.actionbar.valueBar.setPosition(xPos, yPos + h2 / 2);
+  }
+
+  hasBuff(buff: string): boolean {
+    if (!this.curBuff.has(buff)) {
+      return false;
+    }
+    return this.curBuff.get(buff)!;
+  }
+
+  setBuff(buff: string) {
+    if (!this.curBuff.has(buff)) {
+      return;
+    }
+    this.curBuff.set(buff, true);
+
+    setTimeout(() => {
+      this.curDebuff.set(buff, false);
+    }, 15000);
+  }
+
+  setDebuff(debuff: string) {
+    if (!this.curDebuff.has(debuff)) {
+      return;
+    }
+    this.curDebuff.set(debuff, true);
+
+    setTimeout(() => {
+      this.curDebuff.set(debuff, false);
+    }, 15000);
+  }
+
+  hasDebuff(debuff: string): boolean {
+    if (!this.curDebuff.has(debuff)) {
+      return false;
+    }
+    return this.curDebuff.get(debuff)!;
+  }
+
+  draw() {
+    this.healthbar.drawBar();
+    this.actionbar.drawBar();
+  }
+}
+
+export class Player extends Character {
+  actions: Action[];
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    w = 100,
+    h1 = 20,
+    v1 = 100,
+    h2 = 10,
+    v2 = 100
+  ) {
+    super(scene, x, y, w, h1, v1, h2, v2);
+    this.actions = [];
+  }
+
+  isAlive(): boolean {
+    return this.health() > 0;
+  }
+
+  addAction(action: Action) {
+    this.actions.push(action);
+  }
+
+  passTurns() {
+    for (const action of this.actions) {
+      action.turnsPassed += 1;
+    }
+    this.resetAction();
+  }
+
+  setActRate(rate: number) {
+    this.actionbar.setDecrementRate(rate);
+  }
+
+  canAct(): boolean {
+    return this.actionbar.canAct();
+  }
+
+  resetAction() {
+    this.actionbar.resetBar();
+  }
+}
+export class Enemy extends Character {
+  target: number;
+  curAction: number;
+  targetText: Phaser.GameObjects.Text;
+  enemyLabel: Phaser.GameObjects.Text;
+
+  constructor(scene: Phaser.Scene, maxHealth: number) {
+    super(scene, GAME_WIDTH / 2, 60, 750, 30, maxHealth, 30, maxHealth);
+    const enemy = scene.add.image(GAME_WIDTH / 2, 224, "dragon");
+    enemy.setScale(0.5);
+
+    this.target = Math.floor(4 * Math.random());
+    this.curAction = 0;
+
+    this.targetText = scene.add.text(
+      GAME_WIDTH / 2 - 348,
+      70 - 8,
+      `Intent: Claw Attack   Target: Player ${(this.target + 1).toString()}`,
+      { font: "Ariel" }
+    );
+
+    this.targetText.setFontSize(28);
+    this.targetText.setColor("Black");
+    this.targetText.setFontStyle("bold");
+
+    this.enemyLabel = scene.add.text(
+      GAME_WIDTH / 2 - 348,
+      40 - 8,
+      `Enemy: Dragon`,
+      { font: "Ariel" }
+    );
+
+    this.enemyLabel.setFontSize(28);
+    this.enemyLabel.setColor("Black");
+    this.enemyLabel.setFontStyle("bold");
+  }
+
+  selectTarget(allyParty: Player[], target: number = -1) {
+    if (target > -1) {
+      this.target = target;
+      return;
+    }
+
+    let numAlliesDead = 0;
+    for (const allies of allyParty) {
+      if (!allies.isAlive()) {
+        numAlliesDead += 1;
+      }
+    }
+
+    if (numAlliesDead >= allyParty.length) {
+      return;
+    }
+
+    this.target = Math.floor(Math.random() * 4);
+    while (!allyParty[this.target].isAlive()) {
+      this.target = Math.floor(Math.random() * 4);
+    }
+
+    const selector = Math.floor(Math.random() * 10);
+    if (selector < 2) {
+      this.curAction = 1;
+    } else if (selector < 3) {
+      this.curAction = 2;
+    } else {
+      this.curAction = 0;
+    }
+  }
+
+  updateAction(allyParty: Player[]) {
+    this.actionbar.decreaseBar(5);
+
+    if (this.curAction == 0) {
+      if (this.actionbar.getValue() <= 0) {
+        if (this.hasDebuff("Attack")) {
+          allyParty[this.target].damage(10);
+        } else {
+          allyParty[this.target].damage(20);
+        }
+        this.actionbar.resetBar();
+        this.selectTarget(allyParty);
+      }
+    } else if (this.curAction == 1) {
+      if (this.actionbar.getValue() <= 0) {
+        for (const ally of allyParty) {
+          ally.damage(50);
+        }
+        this.actionbar.resetBar();
+        this.selectTarget(allyParty);
+      }
+    } else if (this.curAction == 2) {
+      if (this.actionbar.getValue() <= 0) {
+        if (this.hasDebuff("Attack")) {
+          allyParty[this.target].damage(40);
+        } else {
+          allyParty[this.target].damage(80);
+        }
+        this.actionbar.resetBar();
+        this.selectTarget(allyParty);
+      }
+    }
+  }
+
+  draw() {
+    super.draw();
+    let intent = "Claw Attack";
+    if (this.curAction == 1) {
+      intent = "Firebreath";
+    } else if (this.curAction == 2) {
+      intent = "Rip & Tear";
+    }
+    this.targetText.setText(
+      `Intent: ${intent}   Target: Player ${(this.target + 1).toString()}`
+    );
+  }
+}
+
+interface Bar {
+  valueBar: Phaser.GameObjects.Rectangle;
+  background: Phaser.GameObjects.Rectangle;
+  maxWidth: number;
+  maxValue: number;
+  currentValue: number;
+
+  decreaseBar(amount: number): void;
+  increaseBar(amount: number): void;
+  resetBar(): void;
+  getValue(): number;
+  drawBar(): void;
+}
+
+export class HealthBar implements Bar {
+  valueBar: Phaser.GameObjects.Rectangle;
+  background: Phaser.GameObjects.Rectangle;
+  maxWidth: number;
+  maxValue: number;
+  currentValue: number;
+
+  constructor(
+    scene: Phaser.Scene,
+    xPos: number,
+    yPos: number,
+    width: number,
+    height: number,
+    maxValue: number
+  ) {
+    this.background = new Phaser.GameObjects.Rectangle(
+      scene,
+      xPos,
+      yPos,
+      width,
+      height,
+      0xbab4b4
+    );
+
+    this.valueBar = new Phaser.GameObjects.Rectangle(
+      scene,
+      xPos,
+      yPos,
+      width,
+      height,
+      0xf42c2c
+    );
+
+    scene.add.existing(this.background);
+    scene.add.existing(this.valueBar);
+
+    this.maxWidth = width;
+    this.maxValue = maxValue;
+    this.currentValue = maxValue;
+  }
+
+  decreaseBar(amount: number) {
+    this.currentValue -= amount;
+  }
+
+  increaseBar(amount: number) {
+    this.currentValue += amount;
+    if (this.currentValue > this.maxValue) {
+      this.currentValue = this.maxValue;
+    }
+  }
+
+  resetBar() {
+    this.currentValue = this.maxValue;
+  }
+
+  getValue(): number {
+    return this.currentValue;
+  }
+
+  drawBar() {
+    this.valueBar.width = (this.currentValue / this.maxValue) * this.maxWidth;
+    if (this.valueBar.width < 0) {
+      this.valueBar.width = 0;
+    }
+  }
+}
+
+export class ActionBar extends HealthBar {
+  currentValue: number;
+  decrementRate: number;
+
+  constructor(
+    scene: Phaser.Scene,
+    xPos: number,
+    yPos: number,
+    width: number,
+    height: number,
+    maxValue: number
+  ) {
+    super(scene, xPos, yPos, width, height, maxValue);
+    this.valueBar.setFillStyle(0x0c7fb0);
+    this.currentValue = maxValue;
+    this.decrementRate = 5;
+  }
+
+  canAct(): boolean {
+    return this.currentValue <= 0;
+  }
+
+  setDecrementRate(n: number) {
+    this.decrementRate = n;
+  }
+
+  update() {
+    if (this.currentValue > 0) {
+      this.decreaseBar(this.decrementRate);
+      this.drawBar();
+    }
+  }
+}
--- src/scenes/combat.ts
+++ src/scenes/combat.ts
@@ -1,18 +1,575 @@
 /* Combat Scene: Main combat scene where player faces off against an enemy
  */
 
-import Phaser from "phaser";
+import { Enemy, Player, Character } from "../prefabs/characterElements";
+import { TargetAllyAction, TargetEnemyAction } from "../prefabs/actions";
+import { skillList } from "../prefabs/skills";
+
+const GAME_WIDTH = 1280;
+const GAME_HEIGHT = 720;
+
+let autoMode = false;
+let skillInt = 0;
 
 export class Combat extends Phaser.Scene {
+  private autoText: Phaser.GameObjects.Text;
+  private autoArea: Phaser.GameObjects.Rectangle;
+  player: Player | null;
+  playerHealthText: Phaser.GameObjects.Text | null;
+  skillDescription: Phaser.GameObjects.Text | null;
+  allies: Player[];
+  enemy: Enemy | null;
+
+  dimUI: Phaser.GameObjects.Rectangle | null;
+  enemyDefenseStatus: Phaser.GameObjects.Text | null;
+  enemyAttackStatus: Phaser.GameObjects.Text | null;
+  allyBuffStatus: Phaser.GameObjects.Text | null;
+  allyActionStatus: Phaser.GameObjects.Text[];
+
   constructor() {
-    super("combat");
+    super("Combat");
+
+    this.player = null;
+    this.playerHealthText = null;
+    this.skillDescription = null;
+    this.allies = [];
+    this.enemy = null;
+    this.dimUI = null;
+    this.enemyDefenseStatus = null;
+    this.enemyAttackStatus = null;
+    this.allyBuffStatus = null;
+    this.allyActionStatus = [];
   }
 
   preload() {}
 
   create() {
-    // Test code for scene transition remove
-    console.log("Combat Scene Loaded");
+    this.drawBackground();
+    this.initializeUI();
+    this.initializePlayer();
+    this.initializeAllies();
+    this.enemy = new Enemy(this, 2000);
+
+    const status = this.add.text(GAME_WIDTH / 2 + 20, 92, "Status:");
+    status.setFontSize("28px");
+    status.setColor("black");
+    status.setFontStyle("bold");
+
+    this.enemyDefenseStatus = this.add.text(
+      GAME_WIDTH / 2 + 144 + 96,
+      92,
+      `🛡️⬇️`
+    );
+    this.enemyDefenseStatus.setFontSize("28px");
+    this.enemyDefenseStatus.setColor("black");
+
+    this.enemyAttackStatus = this.add.text(GAME_WIDTH / 2 + 144, 92, `⚔️⬇️`);
+    this.enemyAttackStatus.setFontSize("28px");
+    this.enemyAttackStatus.setColor("black");
+
+    this.allyBuffStatus = this.add.text(
+      GAME_WIDTH / 2 - 48,
+      GAME_HEIGHT - 100 - 58,
+      `⚔️⬆️`
+    );
+    this.allyBuffStatus.setFontSize("28px");
+    this.allyBuffStatus.setColor("black");
+
+    this.initializeActions();
+
+    // auto mode
+    var autoButton = this.add.rectangle(
+      120,
+      80,
+      200,
+      100,
+      0xFFC0CB
+    );
+    autoButton.setInteractive();
+    autoButton.on('pointerup', () => {
+      autoMode = !autoMode;
+  });
+
+    this.autoText = this.add.text(30, 70, 'Auto: OFF', { fontSize: '32px'});
+    this.autoArea = this.add.rectangle(640, 695, 1280, 150, 0xff00ff, 0.5);
+    
   }
-  update() {}
+
+  update() {
+    this.drawCharacters();
+    this.drawSkills();
+    this.player?.actionbar.update();
+    this.enemy?.updateAction(this.allies);
+    this.simulateAllies();
+    this.drawUI();
+
+    // auto mode
+    if(autoMode)
+    {
+      this.autoText.setText("Auto: ON");
+      this.autoArea.setActive(true).setVisible(true);
+      
+      // disable all spells
+
+      // script
+      if (this.player?.canAct())
+      {
+        if(skillInt == 0)
+        {
+          skillList.get("attack")?.effect(this.player!, this.enemy as Character);
+          skillInt = 1;
+        }
+        else if(skillInt == 1)
+        {
+          skillList.get("heal")?.effect(this.player!, this.player as Character);
+          skillInt = 2;
+        }
+        else if(skillInt == 2)
+        {
+          skillList.get("dual strikes")?.effect(this.player!, this.enemy as Character);
+          skillInt = 3;
+        }
+       else if(skillInt == 3)
+        {
+          skillList.get("empower")?.effect(this.player!, this.player as Character);
+          skillInt = 0;
+        }
+        this.player!.resetAction();
+        //console.log("auto attack");
+      }
+      //skillList.get("heal")?.effect(this.player!, this.player as Character);
+      //skillList.get("dual strikes")?.effect(this.player!, this.enemy as Character);
+      // skillList.get("empower")?.effect(this.player!, this.player as Character);
+
+      
+    }
+    else
+    {
+      this.autoText.setText("Auto: OFF");
+      this.autoArea.setActive(false).setVisible(false);
+    }
+    
+
+    if (this.enemy?.hasDebuff("Defense")) {
+      this.enemyDefenseStatus!.alpha = 1;
+    } else {
+      this.enemyDefenseStatus!.alpha = 0;
+    }
+
+    if (this.enemy?.hasDebuff("Attack")) {
+      this.enemyAttackStatus!.alpha = 1;
+    } else {
+      this.enemyAttackStatus!.alpha = 0;
+    }
+
+    if (this.player?.hasBuff("Attack")) {
+      this.allyBuffStatus!.alpha = 1;
+    } else {
+      this.allyBuffStatus!.alpha = 0;
+    }
+
+    if (!this.player?.canAct()) {
+      this.skillDescription?.setText("Waiting...");
+    } else if (
+      this.player?.canAct() &&
+      this.skillDescription?.text == "Waiting..."
+    ) {
+      this.skillDescription?.setText("Ready to Act");
+    }
+
+    if (!this.player?.isAlive() || this.enemy!.health() <= 0) {
+      this.endGame();
+    }
+  }
+
+  initializePlayer() {
+    this.player = new Player(
+      this,
+      GAME_WIDTH / 2,
+      GAME_HEIGHT - 30 - 100,
+      GAME_WIDTH,
+      30,
+      100,
+      30,
+      100
+    );
+    this.allies.push(this.player);
+    this.player.setActRate(1);
+
+    this.playerHealthText = this.add.text(
+      40,
+      GAME_HEIGHT - 58 - 100,
+      `Your (Player 1) Health: ${this.player.health()}/${
+        this.player.healthbar.maxValue
+      }   Status:`
+    );
+    this.playerHealthText.setFontSize("28px");
+    this.playerHealthText.setFontStyle("bold");
+    this.playerHealthText.setColor("black");
+
+    this.skillDescription = this.add.text(40, GAME_HEIGHT - 100 - 26, ``);
+    this.skillDescription.setFontSize("28px");
+    this.skillDescription.setFontStyle("bold");
+    this.skillDescription.setColor("black");
+  }
+
+  initializeAllies() {
+    for (let i = 1; i < 4; i++) {
+      const ally = new Player(
+        this,
+        (GAME_WIDTH * i) / 4,
+        GAME_HEIGHT - GAME_HEIGHT / 3 - 75
+      );
+      this.allies.push(ally);
+
+      this.add.rectangle(
+        (GAME_WIDTH * i) / 4,
+        GAME_HEIGHT - GAME_HEIGHT / 3,
+        100,
+        100,
+        0x79a6d2
+      );
+
+      const text = this.add.text(
+        (GAME_WIDTH * i) / 4 - 40,
+        GAME_HEIGHT - GAME_HEIGHT / 3 - 56,
+        `${i + 1}`
+      );
+      text.setFontSize(128);
+      text.setColor("black");
+
+      const action = this.add.text(
+        (GAME_WIDTH * i) / 4 - 20,
+        GAME_HEIGHT / 2 - 10,
+        `👊`
+      );
+      action.setFontSize(28);
+      action.setColor("black");
+      action.setFontStyle("bold");
+      action.alpha = 0;
+      this.allyActionStatus.push(action);
+    }
+
+    console.log(this.allies.length);
+  }
+
+  initializeActions() {
+    const playBasicAttack = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      GAME_WIDTH / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`attack`)!,
+      this.enemy!
+    );
+
+    playBasicAttack.on("pointerdown", () => {
+      if (playBasicAttack.isUsable()) {
+        this.simulateHit();
+      }
+    });
+
+    this.player?.addAction(playBasicAttack);
+
+    const curClass = localStorage.getItem("playerClass")!;
+    if (curClass == "class 2") {
+      this.initBruiser();
+    } else if (curClass == "class 3") {
+      this.initStriker();
+    } else if (curClass == "class 4") {
+      this.initSupporter();
+    } else {
+      this.initAllRounder();
+    }
+
+    this.dimUI = this.add.rectangle(
+      GAME_WIDTH / 2,
+      GAME_HEIGHT - 50,
+      GAME_WIDTH,
+      100,
+      0x000000,
+      0.2
+    );
+  }
+
+  initAllRounder() {
+    this.player?.addAction(
+      new TargetAllyAction(
+        this,
+        this.skillDescription!,
+        (GAME_WIDTH * 2) / 5,
+        GAME_HEIGHT - 60,
+        this.player,
+        skillList.get(`heal`)!,
+        this.allies,
+        2
+      )
+    );
+
+    this.addDualStrikes();
+    this.addEmpower();
+  }
+
+  initBruiser() {
+    const playDrainBlow = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      (GAME_WIDTH * 2) / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`draining blow`)!,
+      this.enemy!,
+      4
+    );
+
+    playDrainBlow.on("pointerdown", () => {
+      if (playDrainBlow.isUsable()) {
+        this.simulateHit();
+      }
+    });
+
+    const playArmorPierce = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      (GAME_WIDTH * 3) / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`armor pierce`)!,
+      this.enemy!,
+      4
+    );
+
+    playArmorPierce.on("pointerdown", () => {
+      if (playArmorPierce.isUsable()) {
+        this.simulateHit();
+      }
+    });
+
+    this.player?.addAction(playDrainBlow);
+    this.player?.addAction(playArmorPierce);
+    this.addRampage();
+  }
+
+  initStriker() {
+    this.player?.addAction(
+      new TargetEnemyAction(
+        this,
+        this.skillDescription!,
+        (GAME_WIDTH * 2) / 5,
+        GAME_HEIGHT - 60,
+        this.player,
+        skillList.get(`rest`)!,
+        this.enemy!,
+        2
+      )
+    );
+
+    this.addDualStrikes();
+    this.addRampage();
+  }
+
+  initSupporter() {
+    const playWeakening = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      (GAME_WIDTH * 2) / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`weakening blow`)!,
+      this.enemy!,
+      4
+    );
+
+    playWeakening.on("pointerdown", () => {
+      if (playWeakening.isUsable()) {
+        this.simulateHit();
+      }
+    });
+
+    this.player?.addAction(playWeakening);
+
+    this.addDualStrikes();
+    this.addEmpower();
+  }
+
+  addEmpower() {
+    this.player?.addAction(
+      new TargetAllyAction(
+        this,
+        this.skillDescription!,
+        (GAME_WIDTH * 4) / 5,
+        GAME_HEIGHT - 60,
+        this.player,
+        skillList.get(`empower`)!,
+        this.allies,
+        7
+      )
+    );
+  }
+
+  addDualStrikes() {
+    const playDualStrikes = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      (GAME_WIDTH * 3) / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`dual strikes`)!,
+      this.enemy!,
+      5
+    );
+
+    playDualStrikes.on("pointerdown", () => {
+      if (playDualStrikes.isUsable()) {
+        this.simulateHit();
+        setTimeout(() => {
+          this.simulateHit();
+        }, 200);
+      }
+    });
+
+    this.player?.addAction(playDualStrikes);
+  }
+
+  addRampage() {
+    const playRampage = new TargetEnemyAction(
+      this,
+      this.skillDescription!,
+      (4 * GAME_WIDTH) / 5,
+      GAME_HEIGHT - 60,
+      this.player!,
+      skillList.get(`rampage`)!,
+      this.enemy!,
+      7
+    );
+
+    playRampage.on("pointerdown", () => {
+      if (playRampage.isUsable()) {
+        this.simulateHit();
+        setTimeout(() => {
+          this.simulateHit();
+        }, 100);
+        setTimeout(() => {
+          this.simulateHit();
+        }, 200);
+        setTimeout(() => {
+          this.simulateHit();
+        }, 300);
+        setTimeout(() => {
+          this.simulateHit();
+        }, 400);
+      }
+    });
+
+    this.player?.addAction(playRampage);
+  }
+
+  drawBackground() {
+    this.add.rectangle(
+      GAME_WIDTH / 2,
+      GAME_HEIGHT / 2,
+      GAME_WIDTH,
+      GAME_HEIGHT,
+      0xffe6cc
+    );
+  }
+
+  drawCharacters() {
+    this.enemy?.draw();
+    this.player?.draw();
+    this.playerHealthText?.setText(
+      `Your Health: ${this.player!.health()}/${
+        this.player!.healthbar.maxValue
+      }   Status:`
+    );
+    for (const ally of this.allies) {
+      ally.draw();
+    }
+  }
+
+  initializeUI() {
+    this.add.rectangle(
+      GAME_WIDTH / 2,
+      GAME_HEIGHT - 50,
+      GAME_WIDTH,
+      100,
+      0xe6e6e6
+    );
+  }
+
+  drawSkills() {
+    for (const action of this.player?.actions!) {
+      action.draw();
+    }
+  }
+
+  drawUI() {
+    if (this.player?.canAct()) {
+      this.dimUI!.alpha = 0;
+    } else {
+      this.dimUI!.alpha = 1;
+    }
+  }
+
+  endGame() {
+    this.scene.start("End");
+  }
+
+  simulateAllies() {
+    for (let i = 1; i < 4; i++) {
+      const ally = this.allies[i];
+      const action = this.allyActionStatus[i - 1];
+      if (ally.isAlive() && ally.canAct()) {
+        const actChance = Math.random();
+        if (actChance < 0.05) {
+          skillList.get("empower")?.effect(ally, this.player as Character);
+          ally.setActRate(skillList.get("empower")!.actRate);
+          action.setText("⚔️⬆️");
+        } else if (actChance < 0.1) {
+          skillList.get("armor pierce")?.effect(ally, this.enemy as Character);
+          ally.setActRate(skillList.get("armor pierce")!.actRate);
+          action.setText("🛡️⬇️");
+        } else if (actChance < 0.15) {
+          skillList
+            .get("weakening blow")
+            ?.effect(ally, this.enemy as Character);
+          ally.setActRate(skillList.get("weakening blow")!.actRate);
+          action.setText("⚔️⬇️");
+        } else if (actChance < 0.2) {
+          skillList.get("dual strikes")?.effect(ally, this.enemy as Character);
+          ally.setActRate(skillList.get("dual strikes")!.actRate);
+          action.setText("🤺");
+        } else {
+          skillList.get("attack")?.effect(ally, this.enemy as Character);
+          ally.setActRate(skillList.get("attack")!.actRate);
+          action.setText("👊");
+        }
+
+        ally.resetAction();
+
+        action.alpha = 1;
+        setTimeout(() => {
+          action.alpha = 0;
+        }, 500);
+      }
+
+      ally.actionbar.update();
+    }
+  }
+
+  simulateHit() {
+    const randX = 200 * Math.random();
+    const randY = 200 * Math.random();
+    const hitMarker = this.add.text(
+      randX + GAME_WIDTH / 2 - 100,
+      randY + 224 - 100,
+      "👊"
+    );
+    hitMarker.setFontSize("60px");
+
+    setTimeout(() => {
+      hitMarker.destroy();
+    }, 400);
+  }
 }
